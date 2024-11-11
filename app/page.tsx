c4a85609a970d41471ccd11a58bb926714644564
Salem Domani <salem.domani@gmail.com>
2024-11-12 00:14:02 +0300

    chore: enhance home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,17 @@
+import LogoutBtn from "@/components/logout-btn";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
 import Image from "next/image";
+import { redirect } from "next/navigation";
 
-export default function Home() {
+export default async function Home() {
+  const session = await auth.api.getSession({ headers: await headers() });
+
+  if (!session) {
+    return redirect("/login");
+  }
+
   return (
     <main className="flex flex-col items-center justify-center min-h-screen">
       <Image
@@ -12,6 +23,21 @@ export default function Home() {
       />
 
       <h1 className="text-4xl font-bold">Moogle Park</h1>
+      <Avatar>
+        <AvatarImage
+          className="rounded-full"
+          src={session.user.image ?? ""}
+          alt="Profile"
+          width={100}
+          height={100}
+        />
+        <AvatarFallback>
+          {session.user.name.charAt(0)}
+          {session.user.name.charAt(1)}
+        </AvatarFallback>
+      </Avatar>
+      <p className="text-2xl font-bold">Logged in as {session.user.name}</p>
+      <LogoutBtn />
     </main>
   );
 }
