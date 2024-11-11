0b943f3e362bf097e3941d1fecb135d733bf94ab
Salem Domani <salem.domani@gmail.com>
2024-11-12 00:28:20 +0300

    fix: redirect to /auth/login rather than /login

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default async function Home() {
   const session = await auth.api.getSession({ headers: await headers() });
 
   if (!session) {
-    return redirect("/login");
+    return redirect("/auth/login");
   }
 
   return (
