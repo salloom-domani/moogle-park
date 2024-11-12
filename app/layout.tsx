f2ba09d2a0dbdc86c6e548200b89d12c64df21c6
Salem Domani <salem.domani@gmail.com>
2024-11-12 11:14:21 +0300

    fix: root layout font calssName object

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${font} antialiased`}>{children}</body>
+      <body className={`${font.className} antialiased`}>{children}</body>
     </html>
   );
 }
