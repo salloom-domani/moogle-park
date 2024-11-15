745ab44af1ba36f7bd9a19d1896ea6992113d6ec
raneem <your_email@example.com>
2024-11-15 14:39:13 +0300

    desc: add/delete version in version services

--- lib/services/versions.ts
+++ lib/services/versions.ts
@@ -1,5 +1,21 @@
-export function addVersion() {}
+export function addVersion() {
+     //params ownerId, groupId, fileId 
+     //check if in group
+     // check owner of file
+     // chekc if not used
+     // add new data 
+     // update version num
+}
 
-export function deleteVersion() {}
+export function deleteVersion() {
+    //params ownerId, groupId, fileId 
+    // check if in group
+     // check if owner of file
+     // chekc if not used
+     // delete version
 
-export function restoreVersion() {}
+}
+
+export function restoreVersion() {
+
+}
