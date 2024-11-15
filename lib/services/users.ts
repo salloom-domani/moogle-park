0e7264ea4c44fe012d32faa6af6f3c49deadcbfa
raneem <your_email@example.com>
2024-11-15 14:41:41 +0300

    desc: getUsageReport in user services

--- lib/services/users.ts
+++ lib/services/users.ts
@@ -1,1 +1,8 @@
-export function getUsageReport() {}
+export function getUsageReport() {
+    //params userId,ownerId, groupId, fileId 
+    // check userId
+  // check groupId
+  // check fileId
+  // check checkin/out files
+  // check the updates on files
+}
