17f3b7df3a6d293067d4dd93ec91b100c9443abc
raneem <your_email@example.com>
2024-11-15 14:40:58 +0300

    desc: actions in actions services

--- lib/services/actions.ts
+++ lib/services/actions.ts
@@ -1,3 +1,19 @@
-export function addAction() {}
+export function addAction() {
+    // params ownerId, groupId, fileId 
+    // check ownerId
+    // check groupId
+    //check fileId
+  
 
-export function getAllActionsByUser() {}
+}
+
+export function getAllActionsByUser() {
+    // params ownerId,userId, groupId, fileId 
+    // check ownerId
+    // check groupId
+    // check userId
+    // get the created files
+    // get the version of files
+    // get the checkin/out on files
+    // get deleted files
+}
